4af012f48d3aee6afd94d88c5d72d0e2fed1a3f9
Erick Diego <erick.dc.batista@gmail.com>
2024-05-03 18:52:14 -0300

    pastas atualizadas novamente

---
+++ frontend/src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Title } from './styles'
+
+const Footer = () => {
+  return (
+    <>
+        <Title>Footer</Title>
+    </>
+  )
+}
+
+export default Footer
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Title } from './styles'
 import Nav from '../../components/navbar/Nav'
+import Footer from '../../components/footer/Footer'
 
 const Home = () => {
   return (
     <> 
         <Nav/>
         <Title>Home</Title>
+        <Footer/>
     </>
   )
 }
